3c95e2f3f498213605a2d7644a1dbc8955c902ae
Mann Patel <mannpatel3118@gmail.com>
2025-03-22 22:16:44 +0530

    Removed useState in age calculation

--- src/components/home/Subtitle.tsx
+++ src/components/home/Subtitle.tsx
@@ -1,22 +1,21 @@
 "use client";
-import { useEffect, useState } from "react";
 
 const Subtitle = () => {
-  const [age, setAge] = useState<number | null>();
+  // const [age, setAge] = useState<number | null>(20);
 
-  useEffect(() => {
-    const birthDate = new Date("2003-08-01");
-    const currentDate = new Date();
-    const calculatedAge = Math.floor(
-      (currentDate.getTime() - birthDate.getTime()) /
-        (365.25 * 24 * 60 * 60 * 1000)
-    );
-    setAge(calculatedAge);
-  }, []);
+  // useEffect(() => {
+  const birthDate = new Date("2003-03-24");
+  const currentDate = new Date();
+  const calculatedAge = Math.floor(
+    (currentDate.getTime() - birthDate.getTime()) /
+      (365.25 * 24 * 60 * 60 * 1000)
+  );
+  // setAge(calculatedAge);
+  // }, []);
 
   return (
     <div className="text-zinc-600 dark:text-zinc-400">
-      <p>{age}y/o Tech Enthusiast, India</p>
+      <p>{calculatedAge}y/o Tech Enthusiast, India</p>
     </div>
   );
 };
